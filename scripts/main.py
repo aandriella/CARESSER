ad89e2533f1d7ea9d6c0fe20308d5f95074926e8
pal <you@example.com>
2020-08-27 17:26:55 +0200

    Create a version to use for recording when the user interacts with the assistance of the caregier

--- scripts/main.py
+++ scripts/main.py
@@ -22,73 +22,47 @@ import rospy
 class StateMachine(enum.Enum):
 
   def __init__(self, e):
-    self.b_robot_assist_finished = False
-    self.b_robot_feedback_finished = False
+    self.b_caregiver_assist_finished = False
+    self.b_caregiver_feedback_finished = False
     self.b_user_picked_token = False
     self.b_user_placed_token_back = False
     self.b_user_placed_token_sol = False
-    self.b_robot_outcome_finised = False
-    self.b_robot_moved_token_back = False
+    self.b_caregiver_outcome_finished = False
+    self.b_caregiver_moved_token_back = False
     self.b_user_moved_token_back = False
-    self.b_robot_moved_correct_token = False
+    self.b_caregiver_moved_correct_token = False
     self.b_user_reached_max_attempt = False
     self.b_user_reached_timeout = False
-    self.b_robot_reengaged_user = False
+    self.b_caregiver_reengaged_user = False
 
   '''States of the State machine'''
-  S_ROBOT_ASSIST = 1
+  S_CAREGIVER_ASSIST = 1
   S_USER_ACTION = 2
   S_USER_PICK_TOKEN = 3
-  S_ROBOT_FEEDBACK = 4
+  S_CAREGIVER_FEEDBACK = 4
   S_USER_PLACE = 5
   S_USER_PLACE_TOKEN_BACK = 6
   S_USER_PLACE_TOKEN_SOL = 7
   S_USER_MOVE_TOKEN_BACK = 8
-  S_ROBOT_MOVE_TOKEN_BACK = 9
-  S_ROBOT_MOVE_CORRECT_TOKEN = 10
-  S_ROBOT_OUTCOME = 11
+  S_CAREGIVER_MOVE_TOKEN_BACK = 9
+  S_CAREGIVER_MOVE_CORRECT_TOKEN = 10
+  S_CAREGIVER_OUTCOME = 11
   S_USER_TIMEOUT = 12
 
   CURRENT_STATE = 1
 
-  def robot_provide_assistance(self, game, robot):
+  def caregiver_provide_assistance(self):
     '''
-    Robot action of assistance combining speech and gesture
-    Args:
-      game: the game instance
-      robot: robot instance to perform an action
+    Caregiver provides an action of assistance combining speech and gesture
     Return:
        True when the action has been completed
     '''
-
-    print("R_ASSISTANCE")
-    #recall all the information you may need for providing assistance
-    token_sol = game.get_token_sol()
-    tokens_subset = game.get_subset(3)
-    token_row = game.get_token_row()
-    game.robot_assistance = random.randint(0, 5)
-    success = robot.action["assistance"].__call__(lev_id=game.robot_assistance, row=token_row, counter=game.n_attempt_per_token-1, token=token_sol, facial_expression="neutral", tokens=tokens_subset)
-
-    self.b_robot_assist_finished = True
-    self.b_robot_reengaged_user == False
+    print("C_ASSISTANCE")
+    input = raw_input("Please press a key when assistance has been provided")
+    self.b_caregiver_assist_finished = True
+    self.b_caregiver_reengaged_user == False
     self.CURRENT_STATE = self.S_USER_ACTION
-    return self.b_robot_assist_finished
-
-  # def robot_provide_feedback(self):
-  #   '''
-  #   The robot provides a feddback on the grasped token
-  #   :return:
-  #   '''
-  #   print("R_FEEDBACK")
-  #   self.b_robot_feedback_finished = True
-  #   self.CURRENT_STATE = self.S_USER_PLACE
-  #   return self.b_robot_feedback_finished
-
-  # def robot_reengage_user(self):
-  #   '''As the timeout occurred the robot reengages the user'''
-  #   print("RE_ENGAGE USER")
-  #   self.CURRENT_STATE = self.S_USER_ACTION
-  #   self.b_robot_reengaged_user = True
+    return self.b_caregiver_assist_finished
 
   def update_counters(self, game):
     '''
@@ -150,28 +124,28 @@ class StateMachine(enum.Enum):
         self.n_timeout_per_token = 0
 
 
-  def robot_provide_outcome(self, game, robot):
+  def caregiver_provide_outcome(self, game):
     '''
-    Robot provides the user with the outcome of their move
+    Caregiver provides the user with the outcome of their move
     :param self:
     :return:
     '''
     print("R_OUTCOME")
+    input = raw_input("Please press a key when outcome has been provided")
 
     if game.moved_back:
       print("token has been moved back, DO nothing")
       game.outcome = ""
-      self.CURRENT_STATE = self.S_ROBOT_ASSIST
+      self.CURRENT_STATE = self.S_CAREGIVER_ASSIST
       # check if the user reached his max number of attempts
       if game.n_attempt_per_token >= game.n_max_attempt_per_token:
         print("Max attempt reached")
-        self.S_ROBOT_MOVE_CORRECT_TOKEN = True
+        self.S_CAREGIVER_MOVE_CORRECT_TOKEN = True
         self.b_user_reached_max_attempt = True
-        self.robot_move_correct_token(game, robot)
+        self.caregiver_move_correct_token(game)
 
     elif game.detected_token == []:
       print("timeout")
-      robot.action["timeout"].__call__(counter=game.n_attempt_per_token-1, facial_expression="sad")
       game.outcome = 0
       self.CURRENT_STATE = self.S_ROBOT_ASSIST
 
@@ -180,67 +154,69 @@ class StateMachine(enum.Enum):
         print("Max attempt reached")
         self.S_ROBOT_MOVE_CORRECT_TOKEN = True
         self.b_user_reached_max_attempt = True
-        self.robot_move_correct_token(game, robot)
+        self.caregiver_move_correct_token(game)
 
     # get current move and check if it is the one expeceted in the solution list
     elif game.detected_token[0] == game.solution[game.n_correct_move] \
-        and game.detected_token[2] == str(game.solution.index(game.detected_token[0]) + 1):
-      robot.action["congrats"].__call__(counter=game.n_attempt_per_token-1, facial_expression="happy")
+        and game.detected_token[2] == (game.solution.index(game.detected_token[0]) + 1):
       game.outcome = 1
       print("correct_solution ", game.get_n_correct_move())
-      self.CURRENT_STATE = self.S_ROBOT_ASSIST
+      self.CURRENT_STATE = self.S_CAREGIVER_ASSIST
 
     elif game.detected_token[0] != game.solution[game.n_correct_move] \
         or game.detected_token[2] != game.solution.index(game.detected_token[0]) + 1:
       game.outcome = -1
       print("wrong_solution")
-      robot.action["compassion"].__call__(counter=game.n_attempt_per_token-1, facial_expression="sad")
-      self.CURRENT_STATE = self.S_ROBOT_MOVE_TOKEN_BACK
-      self.user_move_back(game, robot)
+      self.CURRENT_STATE = self.S_CAREGIVER_MOVE_TOKEN_BACK
+      self.user_move_back(game)
 
       # check if the user reached his max number of attempts
       if game.n_attempt_per_token >= game.n_max_attempt_per_token:
         print("Max attempt reached")
-        self.S_ROBOT_MOVE_CORRECT_TOKEN = True
+        self.S_CAREGIVER_MOVE_CORRECT_TOKEN = True
         self.b_user_reached_max_attempt = True
-        self.robot_move_correct_token(game, robot)
+        self.caregiver_move_correct_token(game)
 
     self.b_robot_outcome_finished = True
-    return self.b_robot_outcome_finished
+    return self.b_caregiver_outcome_finished
 
-  def robot_move_correct_token(self, game, robot):
-    print("Robot moves the correct token as the user reached the max number of attempts")
+  def caregiver_move_correct_token(self, game):
+    print("Waiting for caregiver to move the correct token ....")
     # get the current solution
-    token = game.get_token_sol()
-    success = robot.action["max_attempt"].__call__(token=token, counter=game.n_attempt_per_token-1, facial_expression="sad")
-    input = raw_input("move the token in the correct position and press a button")
-    self.CURRENT_STATE = self.S_ROBOT_ASSIST
+    token, _from, _to = game.get_token_sol()
+    while (game.detected_token != (token, _from, _to)):
+      pass
+    print("Caregiver moves the correct token as the user reached the max number of attempts")
+    self.CURRENT_STATE = self.S_CAREGIVER_ASSIST
     self.b_user_moved_token_back = True
-    return self.b_robot_moved_token_back
+    return self.b_caregiver_moved_correct_token
 
-  def robot_move_back(self, game, robot):
+  def caregiver_move_back(self, game):
     # user moved the token in an incorrect location
-    # robot moved it back
-    print("Robot moved back the token")
-    success = robot.action["move_back"].__call__(who="robot", token=game.get_token_sol(), counter=game.n_attempt_per_token-1, facial_expression="neutral")
-    self.CURRENT_STATE = self.S_ROBOT_ASSIST
-    self.b_robot_moved_token_back = True
-    return self.b_robot_moved_token_back
+    # caregiver moved it back
+    token_id, token_from = game.detected_token[0],game.detected_token[1]
+    token_to = game.initial_board[game.initial_board.values(token_id)]
+    while(token_from != token_to):
+      pass
+    print("Caregiver moved back the token")
+    self.CURRENT_STATE = self.S_CAREGIVER_ASSIST
+    self.b_caregiver_moved_token_back = True
+    return self.b_caregiver_moved_token_back
 
-  def user_move_back(self, game, robot):
+  def user_move_back(self, game):
     # user moved the token in an incorrect location
     # robot moved it back
     print("User moved back the token")
     #success = robot.action["move_back"].__call__(who="user", token=game.get_token_sol(), counter=game.n_attempt_per_token-1)
     # get the initial location of the placed token and move back there
-    token, _to, _from = game.detected_token
-    while (game.detected_token != [token, _from, _to]):
+    token_id, token_from, token_to = game.detected_token
+    while (game.detected_token != (token_id, token_to, token_from)):
       pass
-    self.CURRENT_STATE = self.S_ROBOT_ASSIST
+    self.CURRENT_STATE = self.S_CAREGIVER_ASSIST
     self.b_user_moved_token_back = True
     return self.b_user_moved_token_back
 
-  def user_action(self, game, robot):
+  def user_action(self, game):
     '''user action state has been divided in three sub methods:
      1. user pick a token
      1.a check for timeout
@@ -269,7 +245,7 @@ class StateMachine(enum.Enum):
     def user_pick_token(sm, game):
       game.react_time_per_token_spec_t0 = time.time()
       print("U_PICK")
-      sm.CURRENT_STATE = sm.S_ROBOT_FEEDBACK
+      sm.CURRENT_STATE = sm.S_CAREGIVER_FEEDBACK
       detected_token, picked, _, _ = game.get_move_event()
       while (not picked and (detected_token == [])):
         if check_move_timeout(game):
@@ -285,15 +261,10 @@ class StateMachine(enum.Enum):
       sm.b_user_picked_token = True
       return sm.b_user_picked_token
 
-    def robot_provide_feedback(sm, game, robot):
-      robot.cancel_action()
-      print("R_FEEDBACK")
+    def caregiver_provide_feedback(sm):
+      print("C_FEEDBACK")
       game.n_sociable_per_token += 1
       game.n_tot_sociable += 1
-      if game.detected_token[0] == game.solution[game.n_correct_move]:
-        robot.action["pick"].__call__(positive=True, counter=game.n_attempt_per_token-1, facial_expression="happy")
-      else:
-        robot.action["pick"].__call__(positive=False, counter=game.n_attempt_per_token-1, facial_expression="confused")
       sm.CURRENT_STATE = sm.S_USER_PLACE
       sm.robot_provided_feeback_finished = True
       return sm.robot_provided_feeback_finished
@@ -310,7 +281,7 @@ class StateMachine(enum.Enum):
       # check where the user will place the token
       def user_place_token_back(sm):
         print("U_PLACE_BACK")
-        sm.CURRENT_STATE = sm.S_ROBOT_OUTCOME
+        sm.CURRENT_STATE = sm.S_CAREGIVER_OUTCOME
         sm.b_user_placed_token_back = True
         game.elapsed_time_per_token_spec_t1 = time.time() - game.elapsed_time_per_token_spec_t0
         game.elapsed_time_per_token_gen_t1 += game.elapsed_time_per_token_spec_t1
@@ -320,7 +291,7 @@ class StateMachine(enum.Enum):
 
       def user_place_token_sol(sm):
         print("U_PLACE_SOL")
-        sm.CURRENT_STATE = sm.S_ROBOT_OUTCOME
+        sm.CURRENT_STATE = sm.S_CAREGIVER_OUTCOME
         sm.b_user_placed_token_sol = True
         game.elapsed_time_per_token_spec_t1 = time.time() - game.elapsed_time_per_token_spec_t0
         game.elapsed_time_per_token_gen_t1 += game.elapsed_time_per_token_spec_t1
@@ -333,10 +304,10 @@ class StateMachine(enum.Enum):
       while (not placed):
         detected_token, _, placed, moved_back = game.get_move_event()
       if (placed and moved_back):
-        sm.CURRENT_STATE = sm.S_ROBOT_OUTCOME
+        sm.CURRENT_STATE = sm.S_CAREGIVER_OUTCOME
         return user_place_token_back(sm)
       elif (placed and not moved_back):
-        sm.CURRENT_STATE = sm.S_ROBOT_OUTCOME
+        sm.CURRENT_STATE = sm.S_CAREGIVER_OUTCOME
         return user_place_token_sol(sm)
       else:
         assert "Unexpected state"
@@ -345,7 +316,7 @@ class StateMachine(enum.Enum):
     # if the user picks a token and SOCIABLE is active
     if user_pick_token(self, game):
       if game.with_SOCIABLE:
-        if robot_provide_feedback(self, game, robot):
+        if caregiver_provide_feedback(self):
           if user_place(self, game):
             return True
           else:
@@ -359,18 +330,18 @@ class StateMachine(enum.Enum):
           print("Something went wrong with user place")
     else:
       print("user_pick_token==False")
-      self.CURRENT_STATE = self.S_ROBOT_OUTCOME
+      self.CURRENT_STATE = self.S_CAREGIVER_OUTCOME
 
   def num_to_func_to_str(self, argument):
     switcher = {
-      0: self.robot_provide_assistance,
-      1: self.robot_reengage_user,
-      2: self.robot_provide_feedback,
+      0: self.caregiver_provide_assistance,
+      1: self.caregiver_reengage_user,
+      2: self.caregiver_provide_feedback,
       3: self.user_action,
-      4: self.robot_provide_outcome,
-      5: self.robot_move_back,
+      4: self.caregiver_provide_outcome,
+      5: self.caregiver_move_back,
       6: self.user_move_back,
-      7: self.robot_move_correct_token
+      7: self.caregiver_move_correct_token
     }
 
     # get the function based on argument
@@ -397,23 +368,18 @@ def main():
 
   # we create the game instance
   game = Game(board_size=(5, 4), task_length=5, n_max_attempt_per_token=4, timeout=10, objective=objective, with_SOCIABLE=with_SOCIABLE)
-  # we create the robot instance
-  speech = Speech(language)
-  face = Face()
-  gesture = None
-  tiago_robot = Robot(speech, sentences_file, face, gesture)
 
   #user_id = raw_input("please, insert the id of the user:")
   path = os.path.abspath(__file__)
   dir_path = os.path.dirname(path)
   parent_dir_of_file = os.path.dirname(dir_path)
-  path_name = parent_dir_of_file + "/log/" + str(user_id)
+  path_name = parent_dir_of_file + "/caregiver_in_the_loop/log/" + str(user_id)
 
   if not os.path.exists(path_name):
     os.makedirs(path_name)
   else:
     user_id = raw_input("The folder already exists, please remove it or create a new one:")
-    path_name = parent_dir_of_file + "/log/" + user_id
+    path_name = parent_dir_of_file + "/caregiver_in_the_loop/log/" + user_id
     if not os.path.exists(path_name):
       os.makedirs(path_name)
 
@@ -443,24 +409,22 @@ def main():
   log_summary.add_row_entry(game.move_info_summary)
   sm = StateMachine(1)
 
-  #tiago_robot.instruction()
-
   while game.get_n_correct_move() < game.task_length:
 
-    if sm.CURRENT_STATE.value == sm.S_ROBOT_ASSIST.value:
-      sm.robot_provide_assistance(game, tiago_robot)
+    if sm.CURRENT_STATE.value == sm.S_CAREGIVER_ASSIST.value:
+      sm.caregiver_provide_assistance()
       game.avg_robot_assistance_per_move += game.robot_assistance
 
     elif sm.CURRENT_STATE.value == sm.S_USER_ACTION.value:
       print("Expected token ", game.solution[game.get_n_correct_move()])
       time_to_act = time.time()
       #game.with_SOCIABLE = random.randint(0,1)
-      sm.user_action(game, tiago_robot)
+      sm.user_action(game)
       game.total_elapsed_time += time.time() - time_to_act
 
-    elif sm.CURRENT_STATE.value == sm.S_ROBOT_OUTCOME.value:
+    elif sm.CURRENT_STATE.value == sm.S_CAREGIVER_OUTCOME.value:
       # these are reported only because the variables are already reset when a correct move occurred
-      sm.robot_provide_outcome(game, tiago_robot)
+      sm.caregiver_provide_outcome(game)
 
       if (game.outcome == 1 or (game.outcome == 0 and game.n_attempt_per_token == game.n_max_attempt_per_token)
           or (game.outcome == -1 and game.n_attempt_per_token == game.n_max_attempt_per_token)):
