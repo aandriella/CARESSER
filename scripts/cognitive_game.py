a6d9829c08e10308474f8f435d8d1fb567e66fe9
Antonio Andriella <aandriella@iri.upc.edu>
2020-09-23 11:37:48 +0200

    Add optinos for different sorting exercises

--- scripts/cognitive_game.py
+++ scripts/cognitive_game.py
@@ -179,12 +179,23 @@ class Game(object):
     board_ = self.current_board[:]
     #remove the empty cells from the current_board
     board_filtered = list(filter(lambda x: x!="0", board_))
+    even_number_list = [num for num in board_filtered if num % 2 == 0]
+    odd_number_list = [num for num in board_filtered if num % 2 == 0]
     if self.objective == "ascending":
       self.solution = sorted(board_filtered)[:n_token]
+    elif self.objective == "ascending_even":
+      self.solution = sorted(even_number_list)[:n_token]
+    elif self.objective == "ascending_odd":
+      self.solution = sorted(odd_number_list)[:n_token]
     elif self.objective == "descending":
       self.solution = sorted(board_filtered, reverse=False)[:n_token]
+    elif self.objective == "descending_even":
+      self.solution = sorted(even_number_list, reverse=False)[:n_token]
+    elif self.objective == "descending_odd":
+      self.solution = sorted(odd_number_list, reverse=False)[:n_token]
     else:
       assert("Game is not defined contact the developer for integrating it")
+      exit(-1)
     return self.solution
 
   #methods to access the variables outside the class
