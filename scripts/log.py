fca0abce855a9ea33e335e3f08b899d2dadf719b
Antonio Andriella <aandriella@iri.upc.educlear>
2020-10-08 16:01:17 +0200

    set log file as caregiver_in_the_loop

--- scripts/log.py
+++ scripts/log.py
@@ -2,39 +2,44 @@
 import os
 import datetime
 import csv
+import pandas as pd
+
 
 class Log():
-	def __init__(self, filename_spec, fieldnames_spec, filename_gen, fieldnames_gen, filename_sum, fieldnames_sum):
-		self.filename_spec = filename_spec
-		self.filename_gen = filename_gen
-		self.filename_sum = filename_sum
-		self.fieldnames_spec = fieldnames_spec
-		self.fieldnames_gen = fieldnames_gen
-		self.fieldnames_sum = fieldnames_sum
+  def __init__(self, filename_spec, fieldnames_spec, filename_gen, fieldnames_gen,
+               filename_sum, fieldnames_sum, filename_bn_vars, fieldnames_bn_vars):
+    self.filename_spec = filename_spec
+    self.filename_gen = filename_gen
+    self.filename_sum = filename_sum
+    self.filename_bn = filename_bn_vars
+    self.fieldnames_spec = fieldnames_spec
+    self.fieldnames_gen = fieldnames_gen
+    self.fieldnames_sum = fieldnames_sum
+    self.fieldnames_bn = fieldnames_bn_vars
 
-	def add_row_entry(self, log_filename, fieldnames, data):
-		with open(log_filename, mode='a') as csv_file:
-			writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-			writer.writerow(data)
-		csv_file.close()
+  def add_row_entry(self, log_filename, fieldnames, data):
+    with open(log_filename, mode='a') as csv_file:
+      writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+      writer.writerow(data)
+    csv_file.close()
 
 
 
 def main():
-	variables = ['token_id', 'from', 'to', 'react_time', 'elapsed_time']
-	entries_value = {'token_id': 17, 'from': 15, 'to': 2, 'react_time': 1.23, 'elapsed_time': 5.02}
-	entries_name = {'token_id': 'token_id', 'from': 'from', 'to': 'to', 'react_time': 'react_time', 'elapsed_time': 'elapsed_time'}
+  variables = ['token_id', 'from', 'to', 'react_time', 'elapsed_time']
+  entries_value = {'token_id': 17, 'from': 15, 'to': 2, 'react_time': 1.23, 'elapsed_time': 5.02}
+  entries_name = {'token_id': 'token_id', 'from': 'from', 'to': 'to', 'react_time': 'react_time', 'elapsed_time': 'elapsed_time'}
 
-	file = '/home/aandriella/pal/cognitive_game_ws/src/carf/caregiver_in_the_loop/log/csv.txt'
-	log = Log(file, fieldnames_spec=variables, fieldnames_gen=variables, fieldnames_sum=variables)
-	# log.add_row_entry(entries_name, log.fieldnames_spec)
-	# log.add_row_entry(entries_value, log.fieldnames_spec)
-	log.read_csv_file(file)
-
-if __name__ == "__main__":
-	main()
-
-
+  file = '/home/aandriella/pal/cognitive_game_ws/src/carf/caregiver_in_the_loop/log/csv.txt'
+  log = Log(file, fieldnames_spec=variables, fieldnames_gen=variables, fieldnames_sum=variables)
+  # log.add_row_entry(entries_name, log.fieldnames_spec)
+  # log.add_row_entry(entries_value, log.fieldnames_spec)
+  log.read_csv_file(file)
 
 if __name__ == "__main__":
-	main()
+  main()
+
+
+
+if __name__ == "__main__":
+  main()
